165f0c534b3c3e644e56b116b327a0424da5f865
Florian <florian.eppel@7fun.de>
2022-12-31 01:40:46 +0100

    cleaned up views a little bit

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -7,6 +7,20 @@ class TransactionsController < ApplicationController
     gon.client_token = generate_client_token
   end
 
+  def create
+    @result = Braintree::Transaction.sale(
+              amount: current_user.cart_total_price,
+              payment_method_nonce: params[:payment_method_nonce])
+    if @result.success?
+      current_user.purchase_cart_products!
+      redirect_to root_url, notice: "Congratulations! Your transaction has been successful!"
+    else
+      flash[:alert] = "Something went wrong while processing your transaction. Please try again!"
+      gon.client_token = generate_client_token
+      render :new
+    end
+  end
+
   private
 
   def generate_client_token
--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -1,5 +1,5 @@
 class Purchase < ApplicationRecord
-	belong_to :product
+	belongs_to :product
 	belongs_to :buyer, class_name:'User'
 
 end
