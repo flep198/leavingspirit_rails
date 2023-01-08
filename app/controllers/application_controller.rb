18822b80a60d12d7bbd1f56869cabf561efe5593
Florian <florian.eppel@7fun.de>
2023-01-08 22:47:58 +0100

    cart and transactions now also work for users who are not logged in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,20 @@
 class ApplicationController < ActionController::Base
 
+    def current_user
+      super || guest_user
+    end
 
+    private
+
+    def guest_user
+     User.find(session[:guest_user_id].nil? ? session[:guest_user_id] = create_guest_user.id : session[:guest_user_id])
+    end
+
+    def create_guest_user
+      u = User.create(:email => "guest_#{Time.now.to_i}#{rand(99)}@example.com")
+      u.save(:validate => false)
+      u
+    end
 
 
 
--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,6 +1,5 @@
 class TransactionsController < ApplicationController
   
-  before_action :authenticate_user!
   before_action :check_cart!
 
   def new
