d0638f23d1d17d4bf8b0ef896966582e5af4b7be
Florian <florian.eppel@7fun.de>
2023-03-19 21:42:14 +0100

    small bugfix in email header

--- app/mailers/order_confirmation_mailer.rb
+++ app/mailers/order_confirmation_mailer.rb
@@ -1,10 +1,9 @@
 class OrderConfirmationMailer < ApplicationMailer
 
 	def order_confirmation_email
-		@user = params[:user]
 		@order = params[:order]
 
-		mail(to:@order.email, subject: "Thanks for your order at Leaving Spirit (Order Nr "+@order.id)
+		mail(to:@order.email, subject: "Thanks for your order at Leaving Spirit (Order Nr "+@order.id.to_s)
 	end
 
 end
