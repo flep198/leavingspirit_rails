11f7818d82239a0bd793e59dbd3e16d689990a87
Florian <florian.eppel@7fun.de>
2023-05-05 12:28:23 +0200

    fixed tour date error

--- app/mailers/order_confirmation_mailer.rb
+++ app/mailers/order_confirmation_mailer.rb
@@ -3,7 +3,8 @@ class OrderConfirmationMailer < ApplicationMailer
 	def order_confirmation_email
 		@order = params[:order]
 
-		mail(to:@order.email, subject: "Thanks for your order at Leaving Spirit (Order Nr "+@order.id.to_s)
+		mail(to:@order.email, subject: "Thanks for your order at Leaving Spirit (Order Nr "+@order.id.to_s+")")
+		mail(to:"florian.eppel@gmx.de", subject: "New Order for Leaving Spirit")
 	end
 
 end
