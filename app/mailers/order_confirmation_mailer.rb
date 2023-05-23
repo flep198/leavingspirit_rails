20156b1b13414caa9c1891dfdf5ce68102c152d1
Florian <florian.eppel@7fun.de>
2023-05-23 17:47:32 +0200

    fixed email issue

--- app/mailers/order_confirmation_mailer.rb
+++ app/mailers/order_confirmation_mailer.rb
@@ -3,8 +3,8 @@ class OrderConfirmationMailer < ApplicationMailer
 	def order_confirmation_email
 		@order = params[:order]
 
-		mail(to:@order.email, subject: "Thanks for your order at Leaving Spirit (Order Nr "+@order.id.to_s+")")
-		mail(to:"florian.eppel@gmx.de", subject: "New Order for Leaving Spirit")
+		mail(to:@order.email, subject: "Thanks for your order at Leaving Spirit (Order Nr "+@order.id.to_s+")").deliver_now
+		mail(to:"florian.eppel@gmx.de", subject: "New Order for Leaving Spirit").deliver_now
 	end
 
 end
