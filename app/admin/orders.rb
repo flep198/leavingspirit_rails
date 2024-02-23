56f77ab75c571bd003bc36020f41c96cd08b1def
Florian <florian.eppel@7fun.de>
2024-02-23 01:59:18 +0100

    changed active admin view for order for better overview of products

--- app/admin/orders.rb
+++ app/admin/orders.rb
@@ -14,5 +14,32 @@ ActiveAdmin.register Order do
   #   permitted << :other if params[:action] == 'create' && current_user.admin?
   #   permitted
   # end
+  show do |order|
+    panel("Order") do
+      table_for(order) do
+        column "Name" do |i|
+          i.first_name.to_s + " " + i.last_name.to_s
+        end
+        column "Address" do |i|
+          i.address_line1.to_s + " " + order.address_line2.to_s + " " + order.country
+        end
+      end
+    end
+    div do      
+      panel("Items") do
+        table_for(order.purchases) do
+          column "Name" do |i| 
+            i.product.name
+          end
+          column "Price" do |i| 
+            i.product.price.to_s
+          end
+          column "Amount" do |i|
+            i.count.to_s
+          end
+        end
+      end
+    end
+  end
   
 end
